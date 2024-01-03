c12c39e0e2bc88e4c41c2054714207805acc9e2d
StpehenChinag <stephenchinag@gmail.com>
2024-01-03 10:07:40 +0100

    query

--- query/index.js
+++ query/index.js
@@ -8,7 +8,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/post", (req, res) => {});
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+  }
+  if (type === "CommentCreated") {
+  }
+});
 
 app.listen(4002, () => {
   console.log("Query Is Listening ");
